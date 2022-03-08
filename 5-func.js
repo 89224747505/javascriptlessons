ecdcf914b8306c3478f0f91f1412c1fbb81ee5ac
zyryanov_ivan <89224747505@mail.ru>
2022-03-08 15:29:16 +0500

    function example

--- 5-func.js
+++ 5-func.js
@@ -4,3 +4,11 @@ const myFunction = (...func) => (func.reduce((f1, f2) => f1 + f2));
 const add = ( x, y ) => (x+y);
 
 console.log(myFunction(add(1,1), add(1,1), add(1,1)));
+
+const loop = (min, max, callback) => {
+    for ( let i = 0; i < max ; i++ ) callback(i);
+}
+
+loop(0, 10, (i) => {
+    console.log("This number is - ", i);
+})
