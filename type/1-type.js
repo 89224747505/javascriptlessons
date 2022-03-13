593ebe2e18a0c7f5c24a37fda3e70bb1dc824667
zyryanov_ivan <89224747505@mail.ru>
2022-03-13 11:18:18 +0500

    types

--- type/1-type.js
+++ type/1-type.js
@@ -1,13 +1,31 @@
 
-const strictFunction = function (st, num, arr, obj) {
-    let s,n,a,o = false;
-    (st.__proto__ === String.prototype) ? s = true : console.log('В st подставте строку');
-    (num.__proto__ === Number.prototype) ? n = true : console.log('В num подставте число');
-    (arr.__proto__ === Array.prototype) ? a = true : console.log('В arr подставте массив');
-    (obj.__proto__ === Object.prototype) ? a = true : console.log('В obj подставте объект');
+
+const checkTypes = (...arg) => (strCompares) => {
+    let result = false;
+    let arrTypes = strCompares.split('/');
+    arg.forEach((value, index) => {
+        if (arrTypes[index] === 'string')
+            (value.__proto__ === String.prototype)
+                ? result = true
+                : console.log(`Переменная ${index+1} не является строкой`);
+        if (arrTypes[index] === 'number')
+            (value.__proto__ === Number.prototype)
+                ? result = true
+                : console.log(`Переменная ${index+1} не является номером`);
+        if (arrTypes[index] === 'array')
+            (value.__proto__ === Array.prototype)
+                ? result = true
+                : console.log(`Переменная ${index+1} не является массивом`);
+        if (arrTypes[index] === 'oblect')
+            (value.__proto__ === Object.prototype)
+                ? result = true
+                : console.log(`Переменная ${index+1} не является объектом`);
+    })
+    return result;
 }
 
+console.log(checkTypes('1', 2, '3', {a:5}, [1,2,3])('string/number/string/object/array'));
 
-strictFunction('123',123,[1,2,3], {a:1, b:2, c:3});
-strictFunction(1,'2',3,{a:4});
+
+
 
