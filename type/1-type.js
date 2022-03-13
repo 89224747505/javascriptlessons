775ec36610814f1a64e18852e8b0ebc95b8a6af6
zyryanov_ivan <89224747505@mail.ru>
2022-03-13 10:47:22 +0500

    types

--- type/1-type.js
+++ type/1-type.js
@@ -1,9 +1,13 @@
 
 const strictFunction = function (st, num, arr, obj) {
-    let go;
-    (typeof st === 'string') ? go = true : go = false;
-    console.log(go);
+    let s,n,a,o = false;
+    (st.__proto__ === String.prototype) ? s = true : console.log('В st подставте строку');
+    (num.__proto__ === Number.prototype) ? n = true : console.log('В num подставте число');
+    (arr.__proto__ === Array.prototype) ? a = true : console.log('В arr подставте массив');
+    (obj.__proto__ === Object.prototype) ? a = true : console.log('В obj подставте объект');
 }
 
 
 strictFunction('123',123,[1,2,3], {a:1, b:2, c:3});
+strictFunction(1,'2',3,{a:4});
+
