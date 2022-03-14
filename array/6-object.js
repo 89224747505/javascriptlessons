545383fce2f2b300f3cb8a20ec5138905e660dad
zyryanov_ivan <89224747505@mail.ru>
2022-03-14 17:52:38 +0500

    types

--- array/6-object.js
+++ array/6-object.js
@@ -3,9 +3,18 @@ const machine = (x, y) => {
     const obj = Object.create(null);
     obj.x = x;
     obj.y = y;
-    obj.moveX = (step) => (this.x +=step);
-    obj.moveY = (step) => (this.y +=step);
-    obj.toString = () => console.log(`Значение x=${this.x}, y=${this.y}`);
+    obj.moveX = function(step) {
+        this.x +=step
+        return this;
+    };
+    obj.moveY = function(step) {
+        this.y +=step
+        return this;
+    };
+    obj.toString = function() {
+        console.log(`Значение x=${this.x}, y=${this.y}`)
+        return this;
+    };
     return obj;
 }
 
@@ -13,4 +22,6 @@ const volvo = machine(0, 0);
 
 volvo.moveX(-1);
 volvo.moveY(100);
+volvo.moveY(-50).moveY(600);
+volvo.moveX(100).moveX(200);
 volvo.toString();
