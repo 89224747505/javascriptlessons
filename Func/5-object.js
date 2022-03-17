b6f32525c8299d050fb1dc90acd673f50e369215
zyryanov_ivan <89224747505@mail.ru>
2022-03-17 19:12:02 +0500

    functions

--- Func/5-object.js
+++ Func/5-object.js
@@ -1,10 +1,21 @@
 const object1 = {
     name:'hallo',
+    age:12,
+    city:"tyumen",
 };
 
 Object.defineProperty(object1, 'property1', {
+    value: 42,
+    enumerable:true,
+    writable: false
+});
+
+Object.defineProperty(object1, 'property2', {
     value: 42,
     writable: false
 });
 
 console.table(object1);
+for (const key in object1) {
+    console.log(key);
+}
