c54e1315626af91823ed90d9d3d32841f1193e7d
zyryanov_ivan <89224747505@mail.ru>
2022-03-17 19:50:09 +0500

    functions

---
+++ Func/6-compose.js
@@ -0,0 +1,15 @@
+
+const compose = (...arg) => {
+    let prevFunction = arg.shift();
+    console.log(prevFunction);
+    arg.forEach(value => {
+        console.log(value);
+    })
+}
+
+const sum = (a, b) => a + b;
+const mul = (a, b) => a * b;
+
+const result = compose(sum(1,2), mul(2,3), sum(3,4), mul(2,2));
+
+console.log(result);
