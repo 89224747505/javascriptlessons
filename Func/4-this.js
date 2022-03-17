288c6562b253bb4e8f5a63642fcf71efd4bbed69
zyryanov_ivan <89224747505@mail.ru>
2022-03-17 18:32:37 +0500

    functions

--- Func/4-this.js
+++ Func/4-this.js
@@ -3,11 +3,18 @@ const Context = function () {
     this.name = "Ivanus Zyranus";
     const city = {
         name: "Tyumen",
-        funcname: this.name,
+        setName:(value) => this.name = value,
+        getName:this.name,
     }
     return city;
 }
 
 const cont = new Context();
+console.log(cont.getName);
+cont.setName("Hallo");
+console.log(cont.getName);
+console.log(cont.name);
 
-console.log(cont);
+(function f1() {
+    console.log("hallo")
+})();
