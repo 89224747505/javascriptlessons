baa75826a43a9588db7f1ceb2371a3c534332bbe
zyryanov_ivan <89224747505@mail.ru>
2022-03-17 18:09:57 +0500

    functions

--- Func/3-func.js
+++ Func/3-func.js
@@ -1,8 +1,12 @@
+const name = "hallo";
 
 const obj = {
     min: (a, b) => a < b ? a : b,
     max: (a, b) => a > b ? a : b,
+    [name]: (a, b) => a * b,
 }
 
 console.log(obj.min(1000,650));
 console.log(obj.max(10,650));
+obj.name = "hallo";
+console.log(obj['hallo'](10,10));
