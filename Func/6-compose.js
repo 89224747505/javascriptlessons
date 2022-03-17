7a87faf005aeb29cf6048c93238f64c7220381f9
zyryanov_ivan <89224747505@mail.ru>
2022-03-17 20:26:43 +0500

    functions

--- Func/6-compose.js
+++ Func/6-compose.js
@@ -1,16 +1,9 @@
 
-const compose = (...arg) => {
-    let prevFunction = arg.shift();
-    console.log(prevFunction);
-    arg.forEach(value => {
-        console.log(value);
-    })
-}
+const compose = (...fn) => x => fn.reverse().reduce((acc, f) => f(acc), x);
 
-const sum = (a, b) => a + b;
-const mul = (a, b) => a * b;
+const sum = (a) => a + a;
+const mul = (a) => a * a;
 
-const result = compose(sum(1,2), mul(2,3), sum(3,4), mul(2,2));
+const result = compose(sum, mul, sum, mul);
 
-console.log(result);
-console.log(result);
+console.log(result(2));
